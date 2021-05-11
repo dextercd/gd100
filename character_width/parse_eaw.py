96cf904e6c17c7cd407adc03fd82e9af78cf090f
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2021-05-11 19:36:43 +0200

    Reuse some parsing code in parse_eaw.py

--- character_width/parse_eaw.py
+++ character_width/parse_eaw.py
@@ -1,12 +1,5 @@
 import range_list
-
-
-def parse_codepoints(codepoints):
-    split = codepoints.split('..')
-    if len(split) == 2:
-        return int(split[0], base=16), int(split[1], base=16)
-
-    return int(codepoints, base=16), int(codepoints, base=16)
+import parse_util
 
 
 def parse(file_name):
@@ -14,20 +7,15 @@ def parse(file_name):
 
     with open(file_name) as data_file:
         for line in data_file:
-            split_result = line.split('#')
+            data = parse_util.split_line(line)
+            if data is None:
+                continue
 
-            if len(split_result) == 0:
-                continue # Empty line
-
-            data = split_result[0].strip()
-            if not data:
-                continue # Only comment on this line
-
-            codepoints, property = data.split(';')
+            codepoints, property = data
             codepoints = codepoints.strip()
             property = property.strip()
 
-            range_start, range_end = parse_codepoints(codepoints)
+            range_start, range_end = parse_util.parse_codepoints(codepoints)
 
             parsed_range = range_list.RangeProperty(range_start, range_end, property)
             ranges.add(parsed_range)
