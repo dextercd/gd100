10d0e7d1f453edfd35e3bf76227c03f2b4e8b0e8
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2021-05-11 21:58:34 +0200

    Build zero width table and use it in character width function

--- character_width/src/character_width.cpp
+++ character_width/src/character_width.cpp
@@ -16,21 +16,73 @@ code_point_range const double_width_ranges[] {
 
 };
 
+code_point_range const zero_width_ranges[] {
+
+#include <zero_width_table.inc>
+
+};
+
+
+// Misc zero width table
+// from https://github.com/jquast/wcwidth
+char32_t const zero_width_misc[]{
+    0,      // Null (Cc)
+    0x034f, // Combining grapheme joiner (Mn)
+    0x200b, // Zero width space
+    0x200c, // Zero width non-joiner
+    0x200d, // Zero width joiner
+    0x200e, // Left-to-right mark
+    0x200f, // Right-to-left mark
+    0x2028, // Line separator (Zl)
+    0x2029, // Paragraph separator (Zp)
+    0x202a, // Left-to-right embedding
+    0x202b, // Right-to-left embedding
+    0x202c, // Pop directional formatting
+    0x202d, // Left-to-right override
+    0x202e, // Right-to-left override
+    0x2060, // Word joiner
+    0x2061, // Function application
+    0x2062, // Invisible times
+    0x2063, // Invisible separator
+};
+
 } // anonymous namespace
 
 int character_width(char32_t code)
 {
-    auto const it = std::lower_bound(
+    auto const zero_width_misc_it = std::find(
+        std::begin(zero_width_misc),
+        std::end(zero_width_misc),
+        code);
+
+    if (zero_width_misc_it != std::end(zero_width_misc))
+        return 0;
+
+    // c0 c1 control codes
+    if (code >= 0x00 && code <= 0x1f || code >= 0x80 && code <= 0x9f)
+        return -1;
+
+    auto const zero_width_it = std::lower_bound(
+        std::begin(zero_width_ranges),
+        std::end(zero_width_ranges),
+        code,
+        [](auto const range, auto const code) { return range.end < code; });
+
+    if (zero_width_it != std::end(zero_width_ranges)) {
+        if (code >= zero_width_it->begin)
+            return 0;
+    }
+
+    auto const double_width_it = std::lower_bound(
         std::begin(double_width_ranges),
         std::end(double_width_ranges),
         code,
         [](auto const range, auto const code) { return range.end < code; });
 
-    if (it == std::end(double_width_ranges))
-        return 1;
-
-    if (code >= it->begin)
-        return 2;
+    if (double_width_it != std::end(double_width_ranges)) {
+        if (code >= double_width_it->begin)
+            return 2;
+    }
 
     return 1;
 }
